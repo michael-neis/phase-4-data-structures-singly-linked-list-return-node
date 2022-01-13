60fd860f18f3b1f4b8810cfd1d9fdd61167e18ba
Michael Neis <michael.neis12@gmail.com>
2022-01-13 16:25:10 -0600

    commit tree 8acaf3cea16d281cdc45b06e8ea4bc6a6801df6f

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,7 +8,56 @@ class LinkedList
   end
 
   def nth_from_end(n)
-    # your code here
+    # if n < 1 || !self.head
+    #   return nil
+    # end
+    # node_array = []
+    # current_node = self.head
+    # while current_node.next_node
+    #   node_array << current_node
+    #   current_node = current_node.next_node
+    # end
+    # node_array << current_node
+    # if n > node_array.length
+    #   return nil
+    # end
+    # index = node_array.length - (n)
+    # node_array[index].value
+
+    # if n < 1 || !self.head
+    #     return nil
+    # end
+    # fast = self.head
+    # slow = self.head
+    # (n-1).times do
+    #   if !fast.next_node
+    #     return nil
+    #   end
+    #   fast = fast.next_node
+    # end
+    # while fast.next_node
+    #   fast = fast.next_node
+    #   slow = slow.next_node
+    # end
+    # slow.value
+
+    if n < 1 || !self.head
+      return nil
+    end
+    count = 1
+    fast = self.head
+    slow = self.head
+    while fast.next_node
+      fast = fast.next_node
+      count += 1
+      if n < count
+        slow = slow.next_node
+      end
+    end
+    if count < n
+      return nil
+    end
+    slow.value
   end
 
 end
